18275e7449079ee92fa903da1960c25eab41dea9
kikaio <gjduddnr5923@naver.com>
2023-09-07 19:33:46 +0900

    no message

--- simpleboard/src/main/java/com/example/simpleboard/config/SimpleBoardSecureConfig.java
+++ simpleboard/src/main/java/com/example/simpleboard/config/SimpleBoardSecureConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
 
 import jakarta.servlet.DispatcherType;
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -21,6 +22,8 @@ public class SimpleBoardSecureConfig {
     @Autowired
     private UserDetailsService userDetailsService;
 
+    private final int maxSessionCnt = 3;
+
     @Bean
     public PasswordEncoder passwordEncoder()
     {
@@ -30,14 +33,22 @@ public class SimpleBoardSecureConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception
     {
+        final String indexUrl = "/";
+
+        final String signInUrl = "/member/sign-in";
+        final String signInProcessUrl = "/member/sign-in-process";
+        final String signInSuccessUrl = "/member/sign-in-success";
+        final String signUpUrl = "/member/sign-up";
+        final String signUpProcessUrl = "/member/sign-up-process";
+        final String signOutUrl = "/member/sign-out"; 
+
         String[] permitAlls = {
-            "/"
-            , "/member/sign-up", "/member/sign-up-process", "/member/sign-in"
+            indexUrl
+            , signUpUrl, signUpProcessUrl, signInUrl
             , "/member/list"
             , "/static", "/status", "/images/**"
         };
         http.cors(cors->cors.disable())
-            .csrf(csrf->csrf.disable())
             .authorizeHttpRequests(req->
             {
                 req.dispatcherTypeMatchers(DispatcherType.FORWARD).permitAll()
@@ -47,6 +58,25 @@ public class SimpleBoardSecureConfig {
                     .anyRequest().authenticated()
                     ;
             })
+            .sessionManagement(sessionConf->{
+                //session max 갯수 지정 및 초과 로그인 시 세션 처리 설정
+                sessionConf.maximumSessions(maxSessionCnt)
+                    .maxSessionsPreventsLogin(true)
+                    ;
+
+                sessionConf.sessionFixation(fixationConfig->{
+                    //session 관리 모드 지정, user가 login 시 session id 발급에 대하여.
+                    fixationConfig.none();
+                    // fixationConfig.changeSessionId();
+                    // fixationConfig.migrateSession();
+                    // fixationConfig.newSession();
+                });
+            })
+            .exceptionHandling(exceptConf->{
+                exceptConf.authenticationEntryPoint((request, response, authException) -> {
+                    response.sendRedirect(signInUrl);
+                });
+            })
             .rememberMe(req->{
                 req.rememberMeCookieName("remember_cookie")
                     .rememberMeParameter("remember")
@@ -54,25 +84,33 @@ public class SimpleBoardSecureConfig {
                     .userDetailsService(userDetailsService)
                     ;
             })
-            .formLogin(req->
+            .formLogin(conf->
             {
-                req.loginPage("/member/sign-in")
-                    .loginProcessingUrl("/member/sign-in-process")
+                conf.loginPage(signInUrl)
+                    .loginProcessingUrl(signInProcessUrl)
                     .usernameParameter("email")
                     .passwordParameter("password")
-                    .defaultSuccessUrl("/member/sign-in-success", true)
+                    .defaultSuccessUrl(signInSuccessUrl, true)
                     .permitAll()
+                    .successHandler((request, response, authentication) -> {
+                            //login success url 말고 직전에 있던 url 로 redirect 처리
+                            HttpSessionRequestCache reqCache = new HttpSessionRequestCache();
+                            var savedReq = reqCache.getRequest(request, response);
+                            String redirecUrl = savedReq.getRedirectUrl();
+                            System.out.println("redirect url : %s".formatted(redirecUrl));
+                            response.sendRedirect(redirecUrl);
+                    })
                 ;
             })
             .logout(req->{
-                req.logoutUrl("/member/sign-out")
+                req.logoutUrl(signOutUrl)
                     .addLogoutHandler((request, response, authentication) -> {
                         var session = request.getSession();
                         if(session != null)
                             session.invalidate();
                     })
                     .logoutSuccessHandler((request, response, authentication) -> {
-                        response.sendRedirect("/member/sign-in");
+                        response.sendRedirect(signInUrl);
                     })
                 ;
             })
