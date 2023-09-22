e229d6d90419d632f0e79dfc9d75a435c6827856
kikaio <gjduddnr5923@naver.com>
2023-09-22 16:55:11 +0900

    기본적인 index page 연결 및 기본 url permit 설정.

---
+++ simpleboard/src/main/java/com/example/simpleboard/config/BoardSecurityConfig.java
@@ -0,0 +1,57 @@
+package com.example.simpleboard.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableMethodSecurity
+public class BoardSecurityConfig {
+
+    @Bean
+    @Order(2)
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception
+    {
+        final String[] permitAllUrls = {
+            "/", "/favicon.ico"
+        };
+        http.authorizeHttpRequests(custom->{
+            custom
+                .requestMatchers(permitAllUrls).permitAll()
+                .anyRequest().denyAll();
+        })
+        ;
+        return http.build();
+    }
+
+    @Bean
+    @Order(1)
+    public SecurityFilterChain exceptSecurityFilterChain(HttpSecurity http) throws Exception
+    {
+        //참고 블로그 : https://bitgadak.tistory.com/11
+        //web ignore 역할 filter chain 추가 하면서 불필요한 filter는 disable 처리.
+        final String[] exceptUrls = {
+            "/static/**", "/favicon.ico"
+        };
+        http.authorizeHttpRequests(custom->{
+            custom
+            .requestMatchers(exceptUrls).permitAll()
+            .anyRequest().permitAll()
+            ;
+        })
+        .requestCache(custom->{
+            custom.disable();
+        })
+        .securityContext(custom->{
+            custom.disable();
+        })
+        .sessionManagement(custom->{
+            custom.disable();
+        })
+        ;
+        return http.build();
+    }
+}
---
+++ simpleboard/src/main/java/com/example/simpleboard/controller/SimpleController.java
@@ -0,0 +1,22 @@
+package com.example.simpleboard.controller;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+@RequestMapping("/")
+public class SimpleController 
+{
+    @GetMapping()
+    public String index(Authentication auth)
+    {
+        if(auth != null)
+            log.info("auth user name[%s]".formatted(auth.getName()));
+        return "index";
+    }    
+}
