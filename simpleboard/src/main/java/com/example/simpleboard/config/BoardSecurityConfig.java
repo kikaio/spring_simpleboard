b26a5dae7d5129fcbe132210f5248964ffb65125
DESKTOP-NV48I2Q\허영욱 <gjduddnr5923@naver.com>
2023-09-29 02:23:30 +0900

    /h2-console 경로 관련 오류 수정  . 단순 문자열로 path 넣어주던 부분 MvcRequestMatcher를 사용하여 어떤 matcher를 사용할 지 명확히 함.

--- simpleboard/src/main/java/com/example/simpleboard/config/BoardSecurityConfig.java
+++ simpleboard/src/main/java/com/example/simpleboard/config/BoardSecurityConfig.java
@@ -6,6 +6,9 @@ import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 import jakarta.servlet.DispatcherType;
 
@@ -15,18 +18,19 @@ public class BoardSecurityConfig {
 
     @Bean
     @Order(2)
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, HandlerMappingIntrospector introspector) throws Exception
     {
-        final String[] permitAllUrls = {
-            "/"
+        final MvcRequestMatcher[] publicMatchers = {
+            new MvcRequestMatcher(introspector, "/")
         };
+        
         http
         .csrf(custom->{ custom.disable();})
         .cors(custom->{ custom.disable();})
         .authorizeHttpRequests(custom->{
             custom
                 .dispatcherTypeMatchers(DispatcherType.FORWARD).permitAll()
-                .requestMatchers(permitAllUrls).permitAll()
+                .requestMatchers(publicMatchers).permitAll()
                 .anyRequest().permitAll(); //member 작업 전까진 모두 permit All 처리.
         })
         ;
@@ -35,22 +39,26 @@ public class BoardSecurityConfig {
 
     @Bean
     @Order(1)
-    public SecurityFilterChain exceptSecurityFilterChain(HttpSecurity http) throws Exception
+    public SecurityFilterChain exceptSecurityFilterChain(HttpSecurity http, HandlerMappingIntrospector introspector) throws Exception
     {
         //참고 블로그 : https://bitgadak.tistory.com/11
         //web ignore 역할 filter chain 추가 하면서 불필요한 filter는 disable 처리.
-        final String[] exceptUrls = {
-            "/static/**", "/favicon.ico"
+        final MvcRequestMatcher[] publicMatchers = {
+            new MvcRequestMatcher(introspector, "/static/**")
+            , new MvcRequestMatcher(introspector, "/favicon.ico")
+            , new MvcRequestMatcher(introspector, "/h2-console/**")
         };
-        http.authorizeHttpRequests(custom->{
+
+        http
+        .csrf(custom->{ custom.disable();})
+        .cors(custom->{ custom.disable();})
+        .authorizeHttpRequests(custom->{
             custom
                 .dispatcherTypeMatchers(DispatcherType.FORWARD).permitAll()
-                .requestMatchers(exceptUrls).permitAll()
+                .requestMatchers(publicMatchers).permitAll()
                 .anyRequest().permitAll()
             ;
         })
-        .csrf(custom->{ custom.disable();})
-        .cors(custom->{ custom.disable();})
         .requestCache(custom->{
             custom.disable();
         })
