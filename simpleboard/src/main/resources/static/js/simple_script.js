815a04df2aab2bd8543a5f746cd3d101a28df380
kikaio <gjduddnr5923@naver.com>
2023-10-13 16:40:05 +0900

    role관련 mustache 파일에 bootstrap 적용

--- simpleboard/src/main/resources/static/js/simple_script.js
+++ simpleboard/src/main/resources/static/js/simple_script.js
@@ -1,3 +1,55 @@
+function doRoleUpdate(role_id)
+{
+    var http = new XMLHttpRequest();
+    var url_to = window.location.origin + "/roles/" + role_id;
+
+    var formData = new FormData();
+    var role_id = document.getElementsByName('role_id')[0].value;
+    var role_name = document.getElementsByName('role_name')[0].value;
+    var role_desc = document.getElementsByName("role_desc")[0].value;
+
+    console.log("role_id : "+ role_id);
+    console.log("role_name : "+ role_name);
+    console.log("role_desc : "+ role_desc);
+
+    formData.append("name", role_name);
+    formData.append("desc", role_desc);
+    console.log("send req body : "+ formData.values);
+
+    fetch(url_to, {
+        method: 'POST'
+        , cache : 'no-cache'
+        , body : formData
+    })
+    .then(res=>{res.json})
+    .then(data=>{console.log(data)})
+    ;
+};
+
+function doUpdate(role_id)
+{
+    var http = new XMLHttpRequest();
+    var url_to = window.location.origin + "/roles/" + role_id;
+    http.open("POST", url_to);
+    http.setRequestHeader("content-type", "application/x-www-form-urlencoded");
+
+    var role_div = document.getElementById("role_div");
+    var role_name = document.getElementsByName('name')[0].value;
+    var role_desc = document.getElementsByName("desc")[0].value;
+    var requset_body = 
+        "name="+role_name
+        + "&"
+        + "desc="+role_desc
+    ;
+    http.addEventListener("readystatechange", function(){
+        if(http.readyState === 4 && http.status === 200)
+        {
+            window.location.reload(true);
+        }
+    });
+    console.log("send req body : "+requset_body);
+    http.send(requset_body);
+};
 function doDeleteComment(board_id, post_id, comment_id)
 {
     var http = new XMLHttpRequest();
